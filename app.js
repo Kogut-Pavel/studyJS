ddbd94b5a6cc70b2097f231bd7e6cdb7dcef8e47
Pavel Kogut <kogut.kogut1@mail.ru>
2022-12-05 21:04:52 +0500

    Выполнен первый урок

---
+++ app.js
@@ -0,0 +1,10 @@
+const money = 10000;
+const income = 5000;
+const addExpenses = '';
+const deposit = true;
+const mission = 50000;
+const period = 12;
+
+alert('Hello world!');
+
+console.log('Hello world!');
